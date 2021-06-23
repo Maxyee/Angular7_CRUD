b6d6fb5913d62ae044627d61575cdf14320fb691
maxyee <eyaminkhan00@gmail.com>
2021-06-23 21:03:11 +0600

    add two package react-router-dom and styled-components. and made the Login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-        <h2>Lets build the linkedIn clone</h2>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Login/>
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
